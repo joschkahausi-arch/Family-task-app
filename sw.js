d48c242a8a7bb11fe284372905aea7e0c152bfeb
joschkahausi-arch <joschkahausi@gmail.com>
2026-02-13 20:54:02 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+﻿const CACHE_NAME = "familytask-cache-v1";
+const urlsToCache = ["familytask.html", "manifest.json", "icon-192.png", "icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
